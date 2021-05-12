1c7c3d51b393080f86431cce88f2f26d6bf321f0
hammad-PC\hammad <hammadansaridon@gmail.com>
2021-05-12 14:24:08 +0500

    firstapp

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(hi());
+}
+
+// ignore: camel_case_types
+class hi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("hi hammad"),
+      ),
+    );
+  }
+}
